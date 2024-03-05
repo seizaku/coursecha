377a5c55e7408e08185d11a521860f77d8a4297a
Zakusei <zakusei.dev@gmail.com>
2024-03-05 11:47:43 +0800

    feat: add bottom navigation bar

--- src/components/bottom-nav.tsx
+++ src/components/bottom-nav.tsx
@@ -11,27 +11,28 @@ type navlinksType = {
 const navlinks: navlinksType[] = [
   {
     href: "/home",
-    icon: <PiHouseDuotone className="h-5 w-5 text-muted-foreground" />,
+    icon: <PiHouseDuotone className="h-5 w-5" />,
   },
   {
     href: "/roadmap",
-    icon: <PiPlantDuotone className="h-5 w-5 text-muted-foreground" />,
+    icon: <PiPlantDuotone className="h-5 w-5" />,
   },
   {
     href: "/chat",
-    icon: <PiChatDuotone className="h-5 w-5 text-muted-foreground" />,
+    icon: <PiChatDuotone className="h-5 w-5" />,
   },
 ];
 
 export const BottomNav = () => {
   const path = usePathname();
   return (
-    <div className="fixed bottom-4 z-20 w-full max-w-sm p-4 md:bottom-12">
+    <div className="fixed bottom-0 z-20 w-full max-w-sm p-4 md:bottom-12">
       <div className="grid h-16 w-full grid-cols-3 place-items-center rounded-full bg-white shadow-md">
-        {navlinks.map((link) => (
+        {navlinks.map((link, index) => (
           <Link
+            key={index}
             href={link.href}
-            className={`flex flex-col items-center rounded-xl p-2 text-xs ${link.href == path ? "bg-secondary" : "hover:bg-secondary"}`}
+            className={`flex flex-col items-center rounded-xl p-2 text-xs transition-all duration-150 ease-in-out ${link.href == path ? "bg-green-50 text-primary" : "text-muted-foreground hover:bg-green-50 hover:text-primary"}`}
           >
             {link.icon}
           </Link>
