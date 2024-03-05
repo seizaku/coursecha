e9bb3f86745cd604237219f9811a774e9a994842
Zakusei <zakusei.dev@gmail.com>
2024-03-05 11:50:05 +0800

    feat: add reusable brand component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@ import { cn } from "@/lib/utils";
 import { GridDotContainer } from "@/components/ui/grid-dot-container";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { BrandName } from "@/components/brand-name";
 
 export default function Home() {
   return (
     <GridDotContainer>
-      <p className="relative z-20 bg-gradient-to-r from-green-400 to-green-300 bg-clip-text py-2 text-4xl font-bold text-transparent sm:text-5xl lg:text-7xl">
-        C O U R S E C H A
-      </p>
+      <BrandName />
       <p className="w-4/6 text-center text-sm text-muted-foreground lg:text-lg">
         Cultivate your future: Learn to micro-farm with{" "}
         <span className="font-semibold text-green-300">KidsWhoFarm</span>
