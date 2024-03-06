4148e0ed5ac4da9e7a57c2014e789d7048c3fb7a
Zakusei <zakusei.dev@gmail.com>
2024-03-06 21:53:51 +0800

    refactor: server url and port

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({ response: "Hello World!" });
 });
 
 app.get("/roadmap", async (req, res) => {
