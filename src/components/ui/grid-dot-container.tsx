10f95b465c87aae23ef20b9938dfdcb996b3b26e
Zakusei <zakusei.dev@gmail.com>
2024-03-06 19:58:27 +0800

    feat: add grid container

--- src/components/ui/grid-dot-container.tsx
+++ src/components/ui/grid-dot-container.tsx
@@ -5,10 +5,10 @@ type Props = {
 
 export const GridDotContainer = ({ children, fade = true }: Props) => {
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="relative flex h-screen w-full flex-col items-center justify-center bg-white bg-grid-small-black/[0.2] dark:bg-black dark:bg-grid-small-white/[0.2]">
+    <main className="flex h-fit flex-col">
+      <div className="relative flex min-h-screen w-full flex-col items-center justify-center bg-background">
         {fade && (
-          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
         )}
         {children}
       </div>
