b01dddb23b8b617cf79d0f15253849c04ba4943e
Zakusei <zakusei.dev@gmail.com>
2024-03-06 20:05:06 +0800

    chore: add meta tag viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
-
+import { Toaster } from "@/components/ui/toaster";
+import type { Viewport } from "next";
 const inter = Inter({ subsets: ["latin"] });
+import { FirebaseAuthProvider } from "@/context/auth-context";
 
 export const metadata: Metadata = {
   title: "Coursecha",
   description: "Kids Who Farm",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#fff",
 };
 
 export default function RootLayout({
@@ -16,7 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <FirebaseAuthProvider>
+          {children}
+          <Toaster />
+        </FirebaseAuthProvider>
+      </body>
     </html>
   );
 }
